6655f03ec2f8ca88a562f0646ff8bcc86678b2e5
theBappy <abappy575@gmail.com>
2025-06-21 13:43:43 +0600

    Custom Ruler and Marker implementation

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -22,12 +22,12 @@ import TextAlign from '@tiptap/extension-text-align'
 // custom extension (created for font-size)
 import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtensions } from "@/extensions/line-height";
-
-
+import { Ruler } from "./Ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore()  
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({editor}){
         setEditor(editor)
     },
@@ -111,6 +111,7 @@ export const Editor = () => {
   });
   return (
     <div className="size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
