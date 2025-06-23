40a5e4303b87674734f2a8130d0ec372864bf18c
theBappy <abappy575@gmail.com>
2025-06-23 11:18:47 +0600

    implements renaming of the file by  updateById in document.ts

--- src/app/(home)/dropdown-selection-menu.tsx
+++ src/app/(home)/dropdown-selection-menu.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ExternalLinkIcon, MoreVertical, TrashIcon } from "lucide-react";
+import { ExternalLinkIcon, FilePenIcon, MoreVertical, TrashIcon } from "lucide-react";
 import { Id } from "../../../convex/_generated/dataModel";
 import {
   DropdownMenu,
@@ -8,6 +8,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { RemoveDialog } from "@/components/remove-dialog";
+import { RenameDialog } from "@/components/rename-dialog";
 
 interface DocumentMenuProps {
   documentId: Id<"documents">;
@@ -28,6 +29,18 @@ export const DocumentMenu = ({
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
+        <RenameDialog
+        documentId={documentId}
+        initialTitle={title}
+        >
+            <DropdownMenuItem
+            onSelect={(e)=> e.preventDefault()}
+            onClick={(e)=> e.stopPropagation()}
+            >
+                <FilePenIcon className="size-4 mr-2" />
+                Rename
+            </DropdownMenuItem>
+        </RenameDialog>
         <RemoveDialog
         documentId={documentId}
         >
