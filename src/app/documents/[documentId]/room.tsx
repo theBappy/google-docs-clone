20ee85115fff74ad15bb2f397a7c1736db2f0e5d
theBappy <abappy575@gmail.com>
2025-06-24 13:06:00 +0600

    notification and inbox implementation

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -8,8 +8,9 @@ import {
 } from "@liveblocks/react";
 import { useParams } from "next/navigation";
 import { FullscreenLoader } from "../../../components/fullscreen-loader";
-import { getUsers } from "./actions";
+import { getUsers, getDocuments } from "./actions";
 import { toast } from "sonner";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 type User = { id: string; name: string; avatar: string };
 
@@ -37,22 +38,37 @@ export function Room({ children }: { children: ReactNode }) {
   return (
     <LiveblocksProvider
       throttle={16}
-      authEndpoint="/api/liveblocks-auth"
-      resolveUsers={({userIds}) => {
+      authEndpoint={async () => {
+        const endpoint = "/api/liveblocks-auth";
+        const room = params.documentId as string;
+
+        const response = await fetch(endpoint, {
+          method: "POST",
+          body: JSON.stringify({ room }),
+        });
+        return await response.json();
+      }}
+      resolveUsers={({ userIds }) => {
         return userIds.map(
           (userId) => users.find((user) => user.id === userId) ?? undefined
-        )
+        );
       }}
-      resolveMentionSuggestions={({text}) => {
+      resolveMentionSuggestions={({ text }) => {
         let filteredUsers = users;
-        if(text){
-          filteredUsers = users.filter((user)=>
-           user.name.toLowerCase().includes(text.toLowerCase())
-          )
+        if (text) {
+          filteredUsers = users.filter((user) =>
+            user.name.toLowerCase().includes(text.toLowerCase())
+          );
         }
-        return filteredUsers.map((user) => user.id)
+        return filteredUsers.map((user) => user.id);
       }}
-      resolveRoomsInfo={() => []}
+      resolveRoomsInfo={async ({ roomIds }) => {
+        const documents = await getDocuments(roomIds as Id<"documents">[]);
+        return documents.map((document) => ({
+          id: document.id,
+          name: document.name,
+        }));
+      }}
     >
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense
